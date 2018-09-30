32a4ee03ce03efd368b94f1dd87d03e6e2bbcd47
MikeyBoss <michaelbosik@gmail.com>
2018-09-30 12:41:36 -0400

    saucers

---
+++ vs-2017/Saucer.h
@@ -0,0 +1,33 @@
+//
+// Saucer.h
+//
+/*
+#include "Object.h"
+#include "EventCollision.h"
+#include <stdlib.h>
+
+class Saucer : public df::Object {
+private:
+	int saucer_health; //health of the saucer (how many bullets it takes to kill)
+	int saucer_type; //an int (0-2) with the types being default, armor, and shooting in that order
+
+	//bullet limitations
+	int fire_slowdown;
+	int fire_countdown;
+
+	df::Sprite *default_saucer; //sprite for the default saucer
+	df::Sprite *armor_saucer; //sprite for the armor saucer
+	df::Sprite *shoot_saucer; //sprite for the shooting saucer
+
+public:
+	Saucer(int type);
+	~Saucer();
+	int eventHandler(const df::Event *p_e);
+	void out();
+	void step();
+	void moveToStart();
+	void fire(df::Vector target);
+	void hit(const df::EventCollision *p_collision_event);
+};
+*/
+
